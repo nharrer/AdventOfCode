a1cdae006ef5b0b6c85637c6faa3ba636d403b0f
Norbert Harrer <nharrer@equilaris.at>
2024-12-07 09:22:03 +0100

    Day07 of 2024

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -13,5 +13,6 @@ pub mod year2024 {
     pub mod day04;
     pub mod day05;
     pub mod day06;
+    pub mod day07;
 }
 
---
+++ rust/src/year2024/day07.rs
@@ -0,0 +1,52 @@
+use std::fs;
+
+const FILENAME: &str = "day07"; // file is in /data/<year>/
+
+struct Equation {
+    result: u64,
+    values: Vec<u64>,
+}
+
+pub fn solve() {
+    let input = fs::read_to_string(FILENAME).expect(&format!("Error loading file: {FILENAME}"));
+    let equations = parse(&input);
+
+    fn add(a: u64, b: u64) -> u64 { a + b }
+    fn mul(a: u64, b: u64) -> u64 { a * b }
+    fn conc(a: u64, b: u64) -> u64 { a * 10_u64.pow((b as f64).log10() as u32 + 1) + b }
+
+    println!("Solution 1: {}", evaluate(&equations, &vec![add, mul]));
+    println!("Solution 2: {}", evaluate(&equations, &vec![add, mul, conc]));
+}
+
+fn evaluate(equations: &Vec<Equation>, operators: &Vec<fn(u64, u64) -> u64>) -> u64 {
+    equations.iter().fold(0, |acc, eq| {
+        let operations = (eq.values.len() - 1) as u32;
+        let opscnt = operators.len();
+        let perms = opscnt.pow(operations);
+        for i in 0..perms {
+            let mut result = eq.values[0];
+            for j in 0..operations {
+                let index = (i / opscnt.pow(j)) % opscnt;
+                let func = operators[index as usize];
+                result = func(result, eq.values[(j + 1) as usize]);
+                if result > eq.result {
+                    break; // a little optimization, since the result only grows
+                }
+            }
+            if result == eq.result {
+                return acc + result;
+            }
+        }
+        acc
+    })
+}
+
+fn parse(input: &str) -> Vec<Equation> {
+    input.lines().map(|line| {
+        let parts = line.split(": ").collect::<Vec<&str>>();
+        let result = parts[0].parse::<u64>().unwrap();
+        let values = parts[1].split_whitespace().map(|x| x.parse::<u64>().unwrap()).collect::<Vec<u64>>();
+        Equation { result, values }
+    }).collect()
+}
