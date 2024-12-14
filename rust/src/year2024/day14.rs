767793a33f14f28d6f27a41ca3108a8d4bcd9cfc
Norbert Harrer <nharrer@equilaris.at>
2024-12-14 13:37:19 +0100

    improved day14 by using entropy

--- rust/src/year2024/day14.rs
+++ rust/src/year2024/day14.rs
@@ -1,8 +1,9 @@
-use std::fs;
-use std::fs::File;
+use std::fs::{self, File};
 use std::borrow::Cow;
 use grid::Grid;
 use gif::{Encoder, Frame, Repeat};
+use std::collections::HashMap;
+use std::f64::consts::E;
 
 const FILENAME: &str = "day14"; // file is in /data/<year>/
 
@@ -18,32 +19,44 @@ pub fn solve() {
 
     let robots = parse(&input);
 
-    println!("Solution 1: {}", solve1(&robots));
+    let (sol1, sol2) = solve1(&robots);
+
+    println!("Solution 1: {}", sol1);
+    println!("Solution 2: {}", sol2);
 }
 
-fn solve1(robots: &Vec<Robot>) -> u32 {
+fn solve1(robots: &Vec<Robot>) -> (u32, u32) {
     let mut robots = robots.clone();
-    let mut robots100 = robots.clone();
+    let mut sol = 0;
 
-    create_gif(0, &robots);
+    let mut min_entropy: (f64, u32) = (1.0, 0);
+
     for s in 1..=STEPS {
         for r in robots.iter_mut() {
             let (p, v) = *r;
             r.0 = move_point(p, v);
         }
-        create_gif(s, &robots);
+
+        let entropy = calculate_entropy(&robots);
+        if entropy < min_entropy.0 {
+            min_entropy = (entropy, s as u32);
+            create_gif(s, &robots);
+        }
+
         if s == 100 {
-            robots100 = robots.clone();
+            let (mh, mv) = (SPACE.0 / 2, SPACE.1 / 2);
+            let quadrants = [
+                ((0, 0), (mh - 1, mv - 1)),
+                ((mh + 1, 0), (SPACE.0 - 1, mv - 1)),
+                ((0, mv + 1), (mh - 1, SPACE.1 - 1)),
+                ((mh + 1, mv + 1), (SPACE.0 - 1, SPACE.1 - 1))
+            ];
+            sol = quadrants.into_iter().map(|((x1, y1), (x2, y2))| {
+                robots.iter().filter(|((x, y), _)| *x >= x1 && *x <= x2 && *y >= y1 && *y <= y2).count()
+            }).fold(1, |acc, c| acc * c as u32)
         }
     }
-
-    let mh = SPACE.0 / 2;
-    let mv = SPACE.1 / 2;
-
-    let quadrants = [((0, 0), (mh - 1, mv - 1)),((mh + 1, 0), (SPACE.0 - 1, mv - 1)),((0, mv + 1), (mh - 1, SPACE.1 - 1)),((mh + 1, mv + 1), (SPACE.0 - 1, SPACE.1 - 1))];
-    quadrants.into_iter().map(|((x1, y1), (x2, y2))| {
-        robots100.iter().filter(|((x, y), _)| *x >= x1 && *x <= x2 && *y >= y1 && *y <= y2).count()
-    }).fold(1, |acc, c| acc * c as u32)
+    (sol, min_entropy.1)
 }
 
 fn move_point(p: Point, v: Point) -> Point {
@@ -52,32 +65,58 @@ fn move_point(p: Point, v: Point) -> Point {
     (x, y)
 }
 
-fn create_gif(s: usize, robots: &Vec<Robot>) {
-    let mut grid:Grid<u32> = Grid::new(SPACE.1 as usize, SPACE.0 as usize);
-    grid.fill(0);
-    for r in robots.iter() {
+fn calculate_entropy(robots: &Vec<Robot>) -> f64 {
+    let grid = robots.iter().fold(Grid::new(SPACE.1 as usize, SPACE.0 as usize), |mut grid, r| {
         *grid.get_mut(r.0.1 as usize, r.0.0 as usize).unwrap() += 1;
+        grid
+    });
+
+    let data: Vec<i32> = grid.iter().cloned().collect();
+    let mut frequency_map: HashMap<i32, usize> = HashMap::new();
+    for &value in &data {
+        *frequency_map.entry(value).or_insert(0) += 1;
+    }
+
+    let total_cells = data.len() as f64;
+    let mut entropy = 0.0;
+    for &count in frequency_map.values() {
+        let probability = count as f64 / total_cells;
+        entropy -= probability * probability.ln() / E.ln();
+    }
+
+    entropy
+}
+
+fn create_gif(s: usize, robots: &Vec<Robot>) {
+    let grid: Grid<i32> = robots.iter().fold(Grid::new(SPACE.1 as usize, SPACE.0 as usize), |mut grid, r| {
+        *grid.get_mut(r.0.1 as usize, r.0.0 as usize).unwrap() += 1;
+        grid
+    });
+
+    let entropy = calculate_entropy(&robots);
+
+    if entropy > 0.194 {
+        return;
     }
 
     let dir = "robots";
     fs::create_dir_all(dir).unwrap();
-
-    // convert grid into gif
     let file = format!("{dir}/output{s}.gif");
 
+    // convert grid into gif
     let mut image = File::create(file.clone()).unwrap();
-    let palette = &[0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF];
+    let palette = &[0x00, 0x00, 0x00, 0x00, 0xFF, 0x00];
     let mut encoder = Encoder::new(&mut image, SPACE.0 as u16, SPACE.1 as u16, palette).unwrap();
     encoder.set_repeat(Repeat::Infinite).unwrap();
 
     let mut frame = Frame::default();
     frame.width = SPACE.0 as u16;
     frame.height = SPACE.1 as u16;
-    let binding = grid.iter().map(|&c| if c == 0 { 0 } else { 255 }).collect::<Vec<u8>>();
+    let binding = grid.iter().map(|&c| if c == 0 { 0 } else { 1 }).collect::<Vec<u8>>();
     frame.buffer = Cow::Borrowed(&binding);
     encoder.write_frame(&frame).unwrap();
 
-    println!("wrote file {}", file);
+    println!("wrote file {}, entrpoy: {}", file, entropy);
 }
 
 fn parse(input: &str) -> Vec<Robot> {
