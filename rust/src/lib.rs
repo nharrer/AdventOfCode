e49a46335b16b7be016f84e8d9be5e4457071fb4
Norbert Harrer <nharrer@equilaris.at>
2024-12-16 16:57:09 +0100

    Improved performance of day 16

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -25,6 +25,7 @@ pub mod year2024 {
     pub mod day14;
     pub mod day15;
     pub mod day16;
+    pub mod day16_v2;
     pub mod day17;
     pub mod day18;
     pub mod day19;
--- rust/src/year2024/day16_v2.rs
+++ rust/src/year2024/day16_v2.rs
@@ -1,6 +1,7 @@
 use grid::Grid;
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{HashMap, HashSet};
 use std::{fs, u32};
+use priority_queue::PriorityQueue;
 
 const FILENAME: &str = "day16"; // file is in /data/<year>/
 const DIR_OFFSET: [(i32, i32); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
@@ -14,16 +15,19 @@ pub fn solve() {
     let mut best: HashMap<(Position, usize), u32> = HashMap::new();
     let (sol1, _) = seek(&grid, player, &mut best, false);
     let (_, sol2) = seek(&grid, player, &mut best, true);
+    println!("Version 2");
     println!("Solution 1: {}", sol1);
     println!("Solution 2: {}", sol2);
 }
 
 fn seek(grid: &Grid<char>, player: Player, best: &mut HashMap<(Position, usize), u32>, part2: bool) -> (u32, usize) {
     let mut min_cost = u32::MAX;
-    let mut paths: HashSet<usize> = HashSet::new();
-    let mut stack: VecDeque<(Player, u32, Vec<usize>)> = VecDeque::new();
-    stack.push_back((player, 0, Vec::new()));
-    while let Some(((pos, dir), current_cost, path)) = stack.pop_front() {
+    let mut paths: HashSet<Position> = HashSet::new();
+    let mut pq = PriorityQueue::new();
+    let vec: Vec<Position> = Vec::new();
+    pq.push((player, vec), 0_i32);
+    while let Some((((pos, dir), path), current_cost)) = pq.pop() {
+        let current_cost = -current_cost as u32;
         let c = *grid.get(pos.0, pos.1).unwrap();
         if c == 'E' {
             if current_cost <= min_cost {
@@ -32,6 +36,7 @@ fn seek(grid: &Grid<char>, player: Player, best: &mut HashMap<(Position, usize),
                 }
                 paths.extend(path);
                 min_cost = current_cost;
+                println!("Found solution with cost: {}", min_cost);
             }
         } else if c != '#' {
             let cost_local_min = *best.get(&(pos, dir)).unwrap_or(&u32::MAX);
@@ -41,10 +46,10 @@ fn seek(grid: &Grid<char>, player: Player, best: &mut HashMap<(Position, usize),
                     let new_pos = ((pos.0 as i32 + DIR_OFFSET[d].0) as usize, (pos.1 as i32 + DIR_OFFSET[d].1) as usize);
                     let path2 = if part2 {
                         let mut path2 = path.clone();
-                        path2.push(new_pos.0 * grid.cols() + new_pos.1);
+                        path2.push(new_pos);
                         path2
                     } else { Vec::new() };
-                    stack.push_back(((new_pos, d), current_cost + cost, path2));
+                    pq.push(((new_pos, d), path2), -((current_cost + cost) as i32));
                 });
             }
         }
