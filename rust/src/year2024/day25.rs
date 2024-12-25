6040e170135990d0e07e2637a0346875f86b4322
Norbert Harrer <nharrer@equilaris.at>
2024-12-25 11:00:30 +0100

    Day 25 of 2024

--- rust/src/year2024/day25.rs
+++ rust/src/year2024/day25.rs
@@ -1,19 +1,38 @@
 use std::fs;
+use grid::Grid;
 
-const FILENAME: &str = "day25_test1"; // file is in /data/<year>/
+const FILENAME: &str = "day25"; // file is in /data/<year>/
 
 pub fn solve() {
-    #[allow(unused_variables)]
     let input = fs::read_to_string(FILENAME).expect(&format!("Error loading file: {FILENAME}"));
 
-    println!("Solution 1: {}", solve1());
-    println!("Solution 2: {}", solve2());
+    let (locks, keys) = parse(&input);
+
+    println!("Solution 1: {}", solve1(&locks, &keys));
+    println!("Solution 2: {}", "Finally done! It was so much fun!");
 }
 
-fn solve1() -> u32 {
-    0
+fn solve1(locks: &Vec<Vec<u8>>, keys: &Vec<Vec<u8>>) -> usize {
+    locks.iter().flat_map(|lock| {
+        keys.iter().filter(move |key| lock.iter().zip(key.iter()).all(|(l, k)| l + k <= 5))
+    }).count()
 }
 
-fn solve2() -> u32 {
-    0
+fn parse(input: &str) -> (Vec<Vec<u8>>, Vec<Vec<u8>>) {
+    let input: String = input.chars().filter(|&c| c != '\r').collect();
+    let (mut locks, mut keys) = (Vec::new(), Vec::new());
+    for block in input.split("\n\n") {
+        let vec: Vec<u8> = block.lines().flat_map(|line| line.chars().map(|c| (c != '.') as u8)).collect();
+        let grid = Grid::from_vec(vec, block.lines().next().unwrap().len());
+        if grid.iter_row(0).map(|&val| val as u8).sum::<u8>() == 5 {
+            locks.push(from_grid(grid));
+        } else {
+            keys.push(from_grid(grid));
+        }
+    }
+    (locks, keys)
+}
+
+fn from_grid(grid: Grid<u8>) -> Vec<u8> {
+    (0..grid.cols()).map(|i| grid.iter_col(i).map(|&val| val as u8).sum::<u8>() - 1).collect()
 }
