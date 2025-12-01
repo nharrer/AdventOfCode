08bc542a183f8ee68af06ed67deb87d534d694a9
Norbert Harrer <nharrer@equilaris.at>
2025-12-01 09:46:57 +0100

    Day 01 of 2025

--- dotnet/2025/Day01/Day01.cs
+++ dotnet/2025/Day01/Day01.cs
@@ -2,6 +2,24 @@ using AdventOfCode;
 
 public class Year2025_Day01(string inputPath) : Solvable($"{inputPath}/input") {
     public override (object, object) Solve() {
-        return ("TODO", "TODO");
+        var turns = _inputLines.Select(line => (line[0], line[1..])).Select(parts => parts.Item1 == 'L' ? -int.Parse(parts.Item2) : int.Parse(parts.Item2));
+        var positions = turns.Aggregate(new List<int> { 50 }, (acc, turn) => { acc.Add((acc.Last() + turn + 100) % 100); return acc; });
+
+        var countZero1 = 0;
+        var countZero2 = 0;
+        var pos = 50;
+        foreach (var turn in turns) {
+            var newpos = pos + turn;
+            countZero2 += Math.Abs(newpos) / 100;   // Count full turns
+            if (pos != 0 && newpos <= 0) {          // Count crossing zero
+                countZero2++;
+            }
+            pos = ((newpos % 100) + 100) % 100;
+            if (pos == 0) {
+                countZero1++;
+            }
+        }
+
+        return (countZero1, countZero2);
     }
 }
