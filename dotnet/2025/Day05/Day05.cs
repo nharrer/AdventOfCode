03313ef3fd73c28ada2d17ce0e77901d28fedb6c
Norbert Harrer <nharrer@equilaris.at>
2025-12-05 14:47:19 +0100

    reduced LoC

--- dotnet/2025/Day05/Day05.cs
+++ dotnet/2025/Day05/Day05.cs
@@ -1,12 +1,9 @@
 using AdventOfCode;
-
 public class Year2025_Day05(string inputPath) : Solvable($"{inputPath}/input") {
-
     public override (object, object) Solve() {
         var parts = _input.Split(LineFeed + LineFeed);
         var rangeSet = parts[0].Split(LineFeed).Select(l => l.Split('-')).Select(p => (long.Parse(p[0]), long.Parse(p[1]))).ToList();
         var ids = parts[1].Split(LineFeed).Select(long.Parse).ToList();
-
         for (int i = 0; i < rangeSet.Count; i++) {
             for (int j = rangeSet.Count - 1; j > i; j--) {
                 var (ri1, ri2) = rangeSet[i]; var (rj1, rj2) = rangeSet[j];
@@ -17,7 +14,6 @@ public class Year2025_Day05(string inputPath) : Solvable($"{inputPath}/input") {
                 }
             }
         }
-
         long cnt1 = ids.Sum(id => rangeSet.Count(r => id >= r.Item1 && id <= r.Item2));
         long cnt2 = rangeSet.Sum(r => r.Item2 - r.Item1 + 1);
         return (cnt1, cnt2);
