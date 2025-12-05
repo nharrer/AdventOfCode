1e34392e703e5a4d2ef9859c6bd4074411660ebf
Norbert Harrer <nharrer@equilaris.at>
2025-12-05 15:03:17 +0100

    Renamed ranges list, because it's not a set anymore.

--- dotnet/2025/Day05/Day05.cs
+++ dotnet/2025/Day05/Day05.cs
@@ -2,20 +2,20 @@ using AdventOfCode;
 public class Year2025_Day05(string inputPath) : Solvable($"{inputPath}/input") {
     public override (object, object) Solve() {
         var parts = _input.Split(LineFeed + LineFeed);
-        var rangeSet = parts[0].Split(LineFeed).Select(l => l.Split('-')).Select(p => (long.Parse(p[0]), long.Parse(p[1]))).ToList();
+        var ranges = parts[0].Split(LineFeed).Select(l => l.Split('-')).Select(p => (long.Parse(p[0]), long.Parse(p[1]))).ToList();
         var ids = parts[1].Split(LineFeed).Select(long.Parse).ToList();
-        for (int i = 0; i < rangeSet.Count; i++) {
-            for (int j = rangeSet.Count - 1; j > i; j--) {
-                var (ri1, ri2) = rangeSet[i]; var (rj1, rj2) = rangeSet[j];
+        for (int i = 0; i < ranges.Count; i++) {
+            for (int j = ranges.Count - 1; j > i; j--) {
+                var (ri1, ri2) = ranges[i]; var (rj1, rj2) = ranges[j];
                 if (ri1 <= rj2 && ri2 >= rj1) {
-                    rangeSet[i] = (Math.Min(ri1, rj1), Math.Max(ri2, rj2));
-                    rangeSet.RemoveAt(j);
-                    j = rangeSet.Count;
+                    ranges[i] = (Math.Min(ri1, rj1), Math.Max(ri2, rj2));
+                    ranges.RemoveAt(j);
+                    j = ranges.Count;
                 }
             }
         }
-        long cnt1 = ids.Sum(id => rangeSet.Count(r => id >= r.Item1 && id <= r.Item2));
-        long cnt2 = rangeSet.Sum(r => r.Item2 - r.Item1 + 1);
+        long cnt1 = ids.Sum(id => ranges.Count(r => id >= r.Item1 && id <= r.Item2));
+        long cnt2 = ranges.Sum(r => r.Item2 - r.Item1 + 1);
         return (cnt1, cnt2);
     }
 }
