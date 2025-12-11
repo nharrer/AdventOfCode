a2e94c95df58cc4fb2b0a33aa9658c8a66a3b5bf
Norbert Harrer <nharrer@equilaris.at>
2025-12-11 08:06:04 +0100

    Day 11 of 2025

---
+++ dotnet/2025/Day11/Day11.cs
@@ -0,0 +1,28 @@
+using AdventOfCode;
+
+public class Year2025_Day11(string inputPath) : Solvable($"{inputPath}/input") {
+
+    public override (object, object) Solve() {
+        var devices = _inputLines.Select(line => line.Split([':', ' '], StringSplitOptions.RemoveEmptyEntries))
+                                 .ToDictionary(parts => parts[0], parts => parts.Skip(1).ToList());
+        devices["out"] = [];
+        var cnt1 = CountAllPaths(devices, "you", "out");
+        var cnt2 = CountAllPaths(devices, "svr", "fft") * CountAllPaths(devices, "fft", "dac") * CountAllPaths(devices, "dac", "out")
+                 + CountAllPaths(devices, "svr", "dac") * CountAllPaths(devices, "dac", "fft") * CountAllPaths(devices, "fft", "out");
+        return (cnt1, cnt2);
+    }
+
+    private long CountAllPaths(Dictionary<string, List<string>> devices, string start, string end) {
+        return CountPaths(devices, start, end, []);
+    }
+
+    private long CountPaths(Dictionary<string, List<string>> devices, string current, string end, Dictionary<string, long> memo) {
+        if (current == end) {
+            return 1;
+        }
+        if (memo.TryGetValue(current, out long cached)) {
+            return cached;
+        }
+        return memo[current] = devices[current].Sum(next => CountPaths(devices, next, end, memo));
+    }
+}
