3f1f4e4a8aefdb47e1fa085c9fb92cfce2034ce3
Norbert Harrer <nharrer@equilaris.at>
2025-12-17 16:16:36 +0100

    Day 17 of 2021

--- 2021/day17/solve-day17.ts
+++ 2021/day17/solve-day17.ts
@@ -6,8 +6,7 @@ const parts = (await readLines(await Deno.open(INPUT_FILE)).next()).value.split(
 const [x0, x1] = parts[2].split('=')[1].split('..').map(Number);
 const [y0, y1] = parts[3].split('=')[1].split('..').map(Number);
 
-let ymaxAsll = 0;
-let found = 0;
+let sol1 = 0, sol2 = 0;
 for (let vx = 1; vx <= x1; vx++) {
     for (let vy = y0; vy <= -y0; vy++) {
         let x = 0, y = 0, vxc = vx, vyc = vy, ymax = 0;
@@ -18,13 +17,13 @@ for (let vx = 1; vx <= x1; vx++) {
             vxc = Math.max(0, vxc - 1);
             vyc--;
             if (x >= x0 && x <= x1 && y >= y0 && y <= y1) {
-                found++;
-                ymaxAll = Math.max(ymaxAll, ymax);
+                sol1 = Math.max(sol1, ymax);
+                sol2++;
                 break;
             }
         }
     }
 }
 
-console.log(`Part one: ${ymaxAll}`);
-console.log(`Part two: ${found}`);
+console.log(`Part one: ${sol1}`);
+console.log(`Part two: ${sol2}`);
